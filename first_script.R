8cfd928c7277d027c4edfbb5ffcecd275ee37842
RoxannaW <roxanna_wijtsma@outlook.com>
2020-09-21 17:04:27 +0200

    "Adding first_script.R"

---
+++ first_script.R
@@ -0,0 +1,15 @@
+# para el ejercicio de hoy copia las siguientes líneas en tu script
+
+# cargamos los datos
+data("mtcars")
+head(mtcars)
+
+# limpio los datos
+datos_limpios <- mtcars %>%
+  filter(cyl == 4)
+
+# grafico los datos
+plot(x = mtcars$wt, y = mtcars$mpg)
+
+library(tidyverse)
+install.packages("tidyverse")
