57c55142f6886a8b5fa2e8cc0a8e02ee3d10c5d1
RoxannaW <roxanna_wijtsma@outlook.com>
2020-09-22 20:35:36 +0200

    start exercises dataframe

---
+++ ejercicios_dataframe.R
@@ -0,0 +1,10 @@
+#1 1. Write a R program to create an empty data frame
+ df_1 <- data.frame()
+ 
+ #2 Write a R program to create a data frame from four given vectors.
+ v1 <- c(4, 5, 6, 7)
+ v2 <- c("a", "b", "c", "d")
+ v3 <- c(T, F, F, T)
+ v4 <- c("blue", "red", "yellow", "green")
+ 
+df_2 <- data.frame(v1, v2, v3, v4)
