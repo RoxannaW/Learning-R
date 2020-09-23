d13cbe1e55059d6dff6438a7d371023f59505088
RoxannaW <roxanna_wijtsma@outlook.com>
2020-09-23 13:21:41 +0200

    finished exercise letra cancion W11D2

---
+++ W11D2ejercicio.R
@@ -0,0 +1,106 @@
+#####################
+# Ejercicio funcion #
+#####################
+
+# Una vez lo termines, subelo a tu github desde R y mandanos el link
+
+### EJERCICIO 
+
+# Crea una funcion llamada mi_cancion que genere la letra de la pegadiza cancion del enlace:
+# https://www.letras.com/pinkfong/baby-shark/
+
+# Encontraras respuesta a algunas de tus preguntas: 
+# https://www.gastonsanchez.com/r4strings/formatting.html
+
+
+mi_cancion <- function(){
+  part_doo <- "doo doo doo doo doo doo"
+  baby <- "Baby shark!"
+  mommy <- "Mommy shark!"
+  daddy <- "Daddy shark!"
+  grandpa <- "Grandpa shark!"
+  grandma <- "Grandma shark!"
+  part_hunt <- "Let's go hunt!"
+  part_run <- "Run away!"
+  part_safe <- "Safe at last!"
+  part_end <- "It’s the end!"
+  
+  for (i in 1:4) {
+    cat("Baby shark, ", part_doo, sep="", "\n")
+    if (i == 4) {
+      cat(baby, "\n", "\n")
+    }
+    
+  }
+  
+  for (i in 1:4) {
+    cat("Mommy shark, ", part_doo, sep="", "\n")
+    if (i == 4) {
+      cat(mommy, "\n", "\n")
+    }
+  }
+  
+  for (i in 1:4) {
+    cat("Daddy shark, ", part_doo, sep="", "\n")
+    if (i == 4) {
+      cat(daddy, "\n", "\n")
+    }
+  }
+  
+  for (i in 1:4) {
+    cat("Grandma shark, ", part_doo, sep="", "\n")
+    if (i == 4) {
+      cat(grandma, "\n", "\n")
+    }
+  }
+  
+  for (i in 1:4) {
+    cat("Grandpa shark, ", part_doo, sep="", "\n")
+    if (i == 4) {
+      cat(grandpa, "\n", "\n")
+    }
+  }
+  
+  for (i in 1:4) {
+    cat("Let's go hunt, ", part_doo, sep="", "\n")
+    if (i == 4) {
+      cat(part_hunt, "\n", "\n")
+    }
+  }
+  
+  for (i in 1:4) {
+    cat("Run away, ", part_doo, sep="", "\n")
+    if (i == 4) {
+      cat(part_hunt, "\n", "\n")
+    }
+  }
+  
+  for (i in 1:4) {
+    cat("safe at last, ", part_doo, sep="", "\n")
+    if (i == 4) {
+      cat(part_safe, "\n", "\n")
+    }
+  }
+  
+  for (i in 1:4) {
+    cat("It's the end, ", part_doo, sep="", "\n")
+    if (i == 4) {
+      cat(part_end, "\n")
+    }
+  }
+
+  
+}
+
+mi_cancion()
+
+
+
+
+
+
+### BONUS
+# Crea una funcion llamada otra_cancion que genere la letra de esta otra cancion:
+# https://es.wikipedia.org/wiki/99_Bottles_of_Beer
+ 
+
--- ejercicios_vector.R
+++ ejercicios_vector.R
@@ -1,58 +1,39 @@
-# 3Write a R program to append value to a given empty vector
-vector_1 <- c()
+#3 Write a R program to append value to a given empty vector.
+vector = c()
 values = c('a','b','c','d','e','f','g')
+
 for (i in 1:length(values))
-  vector_1[i] <- values[i]
-#6 Write a R program to find Sum, Mean and Product of a Vector. 
-vector_2 <- c(1, 4, 5, 6)
-sum(vector_2)
-mean(vector_2)
-prod(vector_2,)
+  
+  vector <- c(vector, values[i]) 
 
-#7 Write a R program to find Sum, Mean and Product of a Vector, ignore element like NA or NaN. 
-sum(vector_2, na.rm = TRUE)
-mean(vector_2, na.rm = TRUE)
-prod(vector_2, na.rm = TRUE)
+#6 Write a R program to find Sum, Mean and Product of a Vector
+vector_1 <- c(1L, 3L, 6L, 10L)
+sum(vector_1)
+mean(vector_1)
+prod(vector_1)
 
-#8Write a R program to find the minimum and the maximum of a Vector.
-min(vector_2)
-max(vector_2)
+#7 Write a R program to find Sum, Mean and Product of a Vector, ignore element like NA or NaN 
+sum(vector_1, na.rm=TRUE)
+mean(vector_1, na.rm=TRUE)
+prod(vector_1, na.rm=TRUE)
 
-#9 Write a R program to sort a Vector in ascending and descending order.
-sort(vector_2, decreasing = FALSE)
-sort(vector_2, decreasing = TRUE)
+#8 Write a R program to find the minimum and the maximum of a Vector.
+min(vector_1)
+max(vector_1)
 
-#10  Write a R program to test whether a given vector contains a specified element
-5 %in% vector_2
+#9 Write a R program to sort a Vector in ascending and descending order
+sort(vector_1, decreasing = FALSE)
+sort(vector_1, decreasing = TRUE)
 
-#12  Write a R program to access the last value in a given vector.
-tail(vector_2, n=1)
+#10 Write a R program to test whether a given vector contains a specified element.
+1 %in% vector_1
 
-#13 Write a R program to find second highest value in a given vector.
-sort(vector_2, decreasing = TRUE)[2]
+#12 Write a R program to access the last value in a given vector.
+tail(vector_1, n=1)
 
-#14 Write a R program to find nth highest value in a given vector.
-n = 1
-sort(vector_2, decreasing = TRUE)[n]
-n = 2
-sort(vector_2, decreasing = TRUE)[n]
-n = 3
-sort(vector_2, decreasing = TRUE)[n]
-n = 4
-sort(vector_2, decreasing = TRUE)[n]
+#13Write a R program to find second highest value in a given vector.
+sort(vector_1, decreasing = TRUE)[2]
 
-
-#16 Write a R program to convert given dataframe column(s) to a vector
+#14 Write a R program to convert given dataframe column(s) to a vector
 data1 <- data.frame(x1 = 1:3, x2 = letters[1:3])
-vec1 <- data1$x1 
-vec2 <- data1$x2 
-
-
-
-#20  Write a R program to reverse the order of given vector.
-vector_1 <- c(5, 7, 9, 6, 5, 1, 5)
-
-rev(vector_1)
-
-#23  Write a R program to convert two columns of a data frame to a named vector
-vector_3 <- tibble::deframe(data1) 
+as_vector <- as.vector(data1)
